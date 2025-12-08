697dbee6c8ea268230241b7cd5bc579c3ae70634
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2025-12-08 20:37:19 +0545

    Add roles and rolebindings endpoint as Terraform output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,6 +7,8 @@ output "provides" {
     secrets          = "secrets",
     service_accounts = "service-accounts",
     pod_defaults     = "pod-defaults",
+    roles            = "roles",
+    role_bindings    = "role-bindings",
   }
 }
 

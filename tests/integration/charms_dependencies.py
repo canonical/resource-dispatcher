cbfacae4ea8607a6ce9cf9a5644af4d1cd1d0b0e
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2026-01-27 17:16:26 +0545

    [PRA-91] Update kubernetes_manifests lib to support secrets (#138)

--- tests/integration/charms_dependencies.py
+++ tests/integration/charms_dependencies.py
@@ -5,3 +5,6 @@ from charmed_kubeflow_chisme.testing import CharmSpec
 METACONTROLLER_OPERATOR = CharmSpec(
     charm="metacontroller-operator", channel="4.11/stable", trust=True
 )
+RESOURCE_DISPATCHER_NO_SECRET = CharmSpec(
+    charm="resource-dispatcher", channel="2.0/stable", trust=True
+)
--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -7,13 +7,16 @@ from pathlib import Path
 
 import lightkube
 from charmed_kubeflow_chisme.kubernetes import KubernetesResourceHandler
+from charmed_kubeflow_chisme.testing import CharmSpec
 from lightkube import codecs
 from lightkube.generic_resource import load_in_cluster_generic_resources
 
 logger = logging.getLogger(__name__)
 
-
 RESOURCE_DISPATCHER_CHARM_NAME = "resource-dispatcher"
+RESOURCE_DISPATCHER_NO_SECRET_REVISION = (
+    402  # Revision that still uses kubernetes_manifests lib 0.1
+)
 
 
 def safe_load_file_to_text(filename: str) -> str:

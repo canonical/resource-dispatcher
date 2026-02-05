d775c578b6ed2cf18851e7e8b4b31263605488dc
MattiaSarti <nmattia1@gmail.com>
2026-02-06 00:13:37 +0100

    Revert "fix: use newer ops"
    This reverts commit 31e8c599b5441ff13d9a48e5d078bf2692e4ff1f.

--- src/charm.py
+++ src/charm.py
@@ -9,12 +9,7 @@ import yaml
 from charmed_kubeflow_chisme.exceptions import ErrorWithStatus, GenericCharmRuntimeError
 from charmed_kubeflow_chisme.kubernetes import KubernetesResourceHandler
 from charmed_kubeflow_chisme.lightkube.batch import delete_many
-from charmed_service_mesh_helpers.models import (
-    Action,
-    AuthorizationPolicySpec,
-    Rule,
-    WorkloadSelector,
-)
+from charmed_kubeflow_chisme.service_mesh import generate_allow_all_authorization_policy
 from charms.istio_beacon_k8s.v0.service_mesh import (
     MeshType,
     PolicyResourceManager,
@@ -23,10 +18,8 @@ from charms.istio_beacon_k8s.v0.service_mesh import (
 from charms.observability_libs.v1.kubernetes_service_patch import KubernetesServicePatch
 from charms.resource_dispatcher.v0.kubernetes_manifests import KubernetesManifestsProvider
 from lightkube import ApiError, Client
-from lightkube.generic_resource import GenericNamespacedResource, load_in_cluster_generic_resources
-from lightkube.models.meta_v1 import ObjectMeta
+from lightkube.generic_resource import load_in_cluster_generic_resources
 from lightkube.models.core_v1 import ServicePort
-from lightkube_extensions.types import AuthorizationPolicy
 from ops import UpgradeCharmEvent, main
 from ops.charm import CharmBase
 from ops.framework import EventBase
@@ -40,30 +33,6 @@ SECRETS_RELATION_NAME = "secrets"
 SERVICEACCOUNTS_RELATION_NAME = "service-accounts"
 ROLES_RELATION_NAME = "roles"
 ROLEBINDINGS_RELATION_NAME = "role-bindings"
-
-
-def generate_allow_all_authorization_policy(
-    app_name: str, namespace: str
-) -> GenericNamespacedResource:
-    """Return an AuthorizationPolicy that allows any workload to talk to specified app.
-
-    Args:
-        app_name: name of the app to allow traffic to
-        namespace: namespace of the app to allow traffic to
-    """
-    return AuthorizationPolicy(
-        metadata=ObjectMeta(
-            name=f"{app_name}-allow-all",
-            namespace=namespace,
-        ),
-        spec=AuthorizationPolicySpec(
-            selector=WorkloadSelector(
-                matchLabels={"app.kubernetes.io/name": app_name},
-            ),
-            action=Action.allow,
-            rules=[Rule()],
-        ).model_dump(by_alias=True, exclude_unset=True, exclude_none=True),
-    )
 
 
 class ResourceDispatcherOperator(CharmBase):

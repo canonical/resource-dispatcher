7fe147521a52f67d5610d8637d7682e74814cce0
deusebio <enrico.deusebio@canonical.com>
2026-06-05 15:28:39 +0200

    fix: swap to use edge version for metacontroller (#165)

--- tests/integration/charms_dependencies.py
+++ tests/integration/charms_dependencies.py
@@ -3,7 +3,7 @@
 from charmed_kubeflow_chisme.testing import CharmSpec
 
 METACONTROLLER_OPERATOR = CharmSpec(
-    charm="metacontroller-operator", channel="4.11/stable", trust=True
+    charm="metacontroller-operator", channel="4.11/edge", trust=True
 )
 RESOURCE_DISPATCHER_NO_SECRET = CharmSpec(
     charm="resource-dispatcher", channel="2.0/stable", trust=True
